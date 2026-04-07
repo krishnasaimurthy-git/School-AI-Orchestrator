68113ecf75d50ca8f9ee43051c15b35db0ff35c7
krishnasaimurthy-git <krishnasaimurthy@gmail.com>
2026-04-07 14:10:43 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "scanner-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/",
+        "index.html",
+        "app.js",
+        "scanner.js",
+        "ocr.js",
+        "pdf.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
