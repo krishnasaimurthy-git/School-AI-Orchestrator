11cac705efc1bf70a334b2fc8b6f3eeb05d7399a
krishnasaimurthy-git <krishnasaimurthy@gmail.com>
2026-04-07 15:11:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,26 @@ async function startCamera() {
   });
   video.srcObject = stream;
 }
-
 function capture() {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   ctx.drawImage(video, 0, 0);
   document.getElementById("status").innerText = "Captured";
 }
+function goToPage(pageId) {
+  document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
+  document.getElementById(pageId).classList.add('active');
+}
+
+function goBack(pageId) {
+  goToPage(pageId);
+}
+
+// Show designation only for School Authority
+document.getElementById("userType").addEventListener("change", function () {
+  if (this.value === "school") {
+    document.getElementById("schoolRole").style.display = "block";
+  } else {
+    document.getElementById("schoolRole").style.display = "none";
+  }
+});
